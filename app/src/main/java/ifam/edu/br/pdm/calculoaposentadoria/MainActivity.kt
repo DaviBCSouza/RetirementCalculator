0f2815ad4d7b047dbd4a17b2ea02f1de298e7a93
DaviBCSouza <contato@davibrenno.com>
2023-11-15 02:05:19 -0400

    Some fixes and optimizations

--- app/src/main/java/ifam/edu/br/pdm/calculoaposentadoria/MainActivity.kt
+++ app/src/main/java/ifam/edu/br/pdm/calculoaposentadoria/MainActivity.kt
@@ -2,11 +2,14 @@ package ifam.edu.br.pdm.calculoaposentadoria
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,13 @@ class MainActivity : Activity() {
         // Spinner
         val spinnerSexo = findViewById<Spinner>(R.id.spinner_sexo)
 
+        val typeface = ResourcesCompat.getFont(this, R.font.chewy)
+
         // Caixa de Idade
         val editTextIdade = findViewById<EditText>(R.id.edit_text_idade)
+
+        // Caixa de Contribuição
+        val editTextContrib = findViewById<EditText>(R.id.edit_text_contrib)
 
         // Botão
         val buttonCalcular = findViewById<Button>(R.id.button_calcular)
@@ -27,12 +35,28 @@ class MainActivity : Activity() {
         // TextView Resultado
         val textViewResultado = findViewById<TextView>(R.id.text_view_resultado)
 
-        // Criando um ArrayAdapter com as opções "Masculino" e "Feminino"
-        val sexoAdapter = ArrayAdapter.createFromResource(
+        // Criando um ArrayAdapter Personalizado
+        val sexoAdapter = object : ArrayAdapter<String>(
             this,
-            R.array.sexo_options,
-            android.R.layout.simple_spinner_dropdown_item
-        )
+            android.R.layout.simple_spinner_item,
+            resources.getStringArray(R.array.sexo_options)
+        ) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getView(position, convertView, parent) as TextView
+                view.typeface = typeface
+                return view
+            }
+
+            override fun getDropDownView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val view = super.getDropDownView(position, convertView, parent) as TextView
+                view.typeface = typeface
+                return view
+            }
+        }
 
         // Especificando o layout a ser usado quando a lista aparecer
         sexoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -49,19 +73,44 @@ class MainActivity : Activity() {
             val idadeTexto = editTextIdade.text.toString()
             val idade = idadeTexto.toIntOrNull()
 
-            // Verificando se a idade é um número válido
-            if (idade != null) {
-                val idadeAposentadoria = if (sexoSelecionado == "Masculino") 65 else 62
-                val anosRestantes = idadeAposentadoria - idade
+            // Obter a contribuição inserida
+            val contribuicaoTexto = editTextContrib.text.toString()
+            val contribuicao = contribuicaoTexto.toIntOrNull()
 
-                if (anosRestantes > 0) {
-                    val mensagem = getString(R.string.mensagem_faltam_anos, anosRestantes)
-                    textViewResultado.text = mensagem
-                } else {
-                    textViewResultado.text = getString(R.string.mensagem_ja_aposentado)
+            // Verificando se a idade e contribuição são números válidos
+            if (idade != null && contribuicao != null) {
+                val idadeAposentadoria = if (sexoSelecionado == "Masculino") 65 else 62
+
+                val anosRestantesIdade = idadeAposentadoria - idade
+                val anosRestantesContribuicao = 15 - contribuicao
+
+                val mensagem = when {
+                    // Pessoa já aposentada
+                    idade >= idadeAposentadoria && contribuicao >= 15 -> getString(R.string.mensagem_ja_aposentado)
+
+                    // Pessoa com contribuição ok, mas idade não
+                    idade <= idadeAposentadoria && contribuicao >= 15 -> getString(
+                        R.string.mensagem_faltam_anos,
+                        anosRestantesIdade
+                    )
+
+                    // Pessoa com idade ok, mas contribuição não
+                    idade >= idadeAposentadoria && contribuicao < 15 -> getString(
+                        R.string.mensagem_faltam_anos,
+                        anosRestantesContribuicao
+                    )
+
+                    // Pessoa com idade e contribuição não suficientes
+                    else -> getString(
+                        R.string.mensagem_faltam_anos,
+                        anosRestantesIdade + anosRestantesContribuicao
+                    )
                 }
+
+                textViewResultado.text = mensagem
+
             } else {
-                textViewResultado.text = getString(R.string.mensagem_idade_invalida)
+                textViewResultado.text = getString(R.string.mensagem_dados_invalidos)
             }
         }
     }
